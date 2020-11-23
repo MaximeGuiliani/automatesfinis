900696095aa6d790032640b77c36d2262dfaf56f
g18020254 <maxime.guiliani@etu.univ-amu.fr>
2020-11-23 12:07:26 +0100

    debut condition

--- tp1-automates.py
+++ tp1-automates.py
@@ -17,11 +17,26 @@ if len(sys.argv) != 3:
 automatonfile = sys.argv[1]  
 word = sys.argv[2]
 
-a = automaton.Automaton("abc")
-a.add_transition("0","x","2 ")
-a.add_transition("0","y","3")
-a.make_accept("3")
-a.to_graphviz("abc-test")
-print(a)
-#print("TODO: faire le TP 1")  
+#a = automaton.Automaton("abc")
+#a.add_transition("0","x","2 ")
+#a.add_transition("0","y","3")
+#a.make_accept("3")
+#a.to_graphviz("abc-test")
+#print(a)
 
+fileName="Document.txt"
+crimefile = open(fileName, 'r')
+#yourResult = [line.split(' ') for line in crimefile.readlines()]
+yourResult = [line.strip("\n") or line.split(" ") for line in crimefile.readlines()]
+
+i=-1
+for x in yourResult:
+    i=i+1
+    for y in yourResult[i]:
+        if y != "0":
+            print("YES")            
+        else:
+            print("ERROR")
+            
+
+#print("TODO: faire le TP 1")  
